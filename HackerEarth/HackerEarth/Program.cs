2184c15ac87b76dbdad77d85260e9bee5c75b832
Karki <Sajag.Karki@tdwilliamson.com>
2022-05-10 10:48:26 -0600

    Resolved problems for Abhay's Review

--- HackerEarth/HackerEarth/Program.cs
+++ HackerEarth/HackerEarth/Program.cs
@@ -1,23 +1,44 @@
 ﻿// See https://aka.ms/new-console-template for more information
-/*Welcome to the world of Java! In this challenge, we practice printing to stdout.
 
-The code stubs in your editor declare a Solution class and a main method. Complete the main method by copying the two lines of code below and pasting them inside the body of your main method.
 
-System.out.println("Hello, World.");
-System.out.println("Hello, Java.");
-Input Format
-
-There is no input for this challenge.
-
-Output Format
-
-You must print two lines of output:
+/*Problem 1
+ * You must print two lines of output:
 
 Print Hello, World. on the first line.
-Print Hello, Java. on the second line.
-Sample Output
+Print Hello, Java. on the second line. */
 
-Hello, World.
-Hello, Java.*/
 
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, C#!");
+
+//Problem 2
+//Given an integer,n , perform the following conditional actions:
+
+//If n is odd, print Weird -Done
+
+int n = 13;
+if (n % 2 == 1)
+{
+    Console.WriteLine("Wierd because I am odd");
+}
+
+//If n is even and in the inclusive range of 2 to 5 , print Not Weird- Done
+if (n % 2 == 0 && n > 1 && n < 6)
+{
+    Console.WriteLine("Not Wierd because I am even & between 2 to 5 ");
+}
+
+//If n is even and in the inclusive range of 6 to 20 , print Weird- Done
+if (n % 2 == 0 && n > 5 && n < 21)
+{
+    Console.WriteLine("Wierd because I am even and between 6 to 20");
+}
+//If n is even and greater than 20 , print Not Weird
+if (n % 2 == 0 && n > 20)
+{
+    Console.WriteLine("Not Wierd");
+}
+
+//Complete the stub code provided in your editor to print whether or not n is weird. Done
+
+
