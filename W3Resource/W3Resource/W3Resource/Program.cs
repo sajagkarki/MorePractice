d7b4dee0245212b8987fff78e84d565f35400ab0
Karki <Sajag.Karki@tdwilliamson.com>
2022-06-07 10:02:14 -0600

    Added new solutions

--- W3Resource/W3Resource/W3Resource/Program.cs
+++ W3Resource/W3Resource/W3Resource/Program.cs
@@ -157,24 +157,48 @@
 
 //Exercise 13 Write a C# program that takes a number as input and then displays a rectangle of 3 columns wide and 5 rows tall using that digit.
 
-int exercise13;
-Console.WriteLine("Enter a number");
 
-exercise13 = int.Parse(Console.ReadLine());
-Console.WriteLine();
 
-for (int i = 1; i <= 3; i++)
-{
-    Console.Write(exercise13 + " ");
-}
-for (int i = 1; i <= 4; i++)
-{
-    Console.WriteLine(exercise13);
-}
-for (int i = 1; i <= 3; i++)
-{
-    Console.Write(exercise13 + " ");
-}
+
+//Step 1- Make three columns manually
+
+//Step 2- Repeat loop 5 times
+//int exercise13;
+//Console.WriteLine("Enter your number ");
+//exercise13 = int.Parse(Console.ReadLine());
+
+//for (int j = 0; j < 5; j++)
+//{
+//    for (int i = 0; i < 3; i++)
+//    {
+//        Console.Write(exercise13);
+//        Console.Write(" ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+
+
+
+
+//Console.WriteLine("Enter a number");
+
+//exercise13 = int.Parse(Console.ReadLine());
+//Console.WriteLine();
+
+//for (int i = 1; i <= 3; i++)
+//{
+//    Console.Write(exercise13 + " ");
+//}
+//for (int i = 1; i <= 4; i++)
+//{
+//    Console.WriteLine(exercise13);
+//}
+//for (int i = 1; i <= 3; i++)
+//{
+//    Console.Write(exercise13 + " ");
+//}
 
 //int x;
 
@@ -186,3 +210,176 @@ for (int i = 1; i <= 3; i++)
 //Console.WriteLine("{0} {0}", x);
 //Console.WriteLine("{0} {0}", x);
 //Console.WriteLine("{0}{0}{0}", x);
+
+//Exercise 14 Write a C# program to convert from celsius degrees to Kelvin and Fahrenheit. 
+//Celcius to Fahrenheight
+//Console.WriteLine("Enter Degrees Celcius");
+//int celcius = int.Parse(Console.ReadLine()); 
+//int farenheit = celcius*9/5 + 32;
+
+
+////Celcius to Kelvin
+//double kelvin = celcius + 273.15; 
+
+//Console.WriteLine(celcius + " degrees celcius is = "+ farenheit + " Degrees farenheit");
+//Console.WriteLine(celcius + " degrees celcius is = " + kelvin + " kelvin");
+
+//Exercise 15 Write a C# program to remove specified a character from a non-empty string using index of a character.//Question not clear
+// Create one string, remove any one character from the string. For example in "Utah" remove "a"
+//Index will always be integer.
+//Two methods of removing . 1.Only with index 2. Other is index with count
+//string characterManipulation = "Utah";
+//string removeCharacter = "a";
+////Remove a from utah
+
+//int index= characterManipulation.IndexOf(removeCharacter);
+//Console.WriteLine(index);
+//string result = characterManipulation.Remove(index, 1); 
+//Console.WriteLine(result);
+
+
+//Exercise 16 Write a C# program to create a new string from a given string where the first and last characters will change their positions.
+//What happens when there is only one character. How do you handle the exception? 
+//Check for boundry condition(one character)
+string firstString = "palindrome";
+
+
+//char firstCharacter = firstString[0];
+//char lastCharacter = firstString[firstString.Length - 1];
+
+//firstString[0] = lastCharacter;
+
+//string result = $"{lastCharacter}{firstString.Substring(1, firstString.Length - 2)}{firstCharacter}";
+
+//If length of string is less than 2 then
+
+
+//if (firstString.Length<2)
+//{
+//    Console.WriteLine(firstString);
+//}
+//else
+//{
+//    string result = $"{firstString[firstString.Length - 1]}{firstString.Substring(1, firstString.Length - 2)}{firstString[0]}";
+//    Console.WriteLine(result);
+//}
+
+//17. Write a C# program to create a new string from a given string (length 1 or more ) with the first character added at
+//the front and back
+
+//I went on cancun vacation. 
+//Output= II went on cancun vacation.I
+
+
+//string exerciseSeventeen = "I went on cancun vacation";
+//Console.Write(exerciseSeventeen[0]);
+//Console.Write(exerciseSeventeen);
+//Console.WriteLine(exerciseSeventeen[0]);
+
+//Console.WriteLine(firstString.Substring(1,firstString.Length-2));
+//Console.WriteLine(result);
+
+//expected result new string is reverse "gajas"
+
+//swop index 0 to index 4
+
+//firststring[0]
+//char temphold = firststring[0];
+//temphold = firststring[4]
+
+//string reverestring;
+
+//18. Write a C# program to check two given integers and return true if one is negative and one is positive.
+
+//int a = 1;
+//int b = 2;
+
+//if (a>=0 && b<0)
+//{
+//    Console.WriteLine("True");
+//}
+//else if (b >= 0 && a < 0)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+//19. Write a C# program to compute the sum of two given integers, if two values are equal then return the triple of their sum.
+
+//int integerOne = 2;
+//int integerTwo = 200;
+
+//int sum = integerOne + integerTwo; 
+
+//if (integerOne==integerTwo)
+//{
+//    Console.WriteLine(sum*3);
+//}
+
+//else
+//{
+//    Console.WriteLine(sum);
+//}
+//Practice One at least every day so you will have practice to Write something, practice to Read something, practice to think something
+//Whatever issue can be resolved on Satruday/Sunday
+//Revise all answers every day
+
+// 20. Write a C# program to get the absolute value of the difference between two given numbers.
+// Return double the absolute value of the difference if the first number is greater than second number.
+
+//int exerciseTwentyFirstNumber = 20; 
+//int exerciseTwentySecondNumber = 5;
+
+//int differenceAbsolute; 
+
+//if (exerciseTwentyFirstNumber>exerciseTwentySecondNumber)
+//{
+//    differenceAbsolute = (exerciseTwentyFirstNumber - exerciseTwentySecondNumber) * 2; 
+//    Console.WriteLine( differenceAbsolute);
+//}
+//else
+//{
+//    Console.WriteLine(exerciseTwentySecondNumber-exerciseTwentyFirstNumber);
+//}
+
+//21. Write a C# program to check the sum of the two given integers and return true if one of the integer is 20 or if their sum is 20. 
+
+//int exerciseTwentyOneFirstNumber = 10;
+//int exercisetwentyOneSecondumber = 0;
+
+//int sumExerciseTwentyOne = exerciseTwentyOneFirstNumber + exercisetwentyOneSecondumber;
+
+//if (exerciseTwentyOneFirstNumber==20 ||exercisetwentyOneSecondumber==20 || sumExerciseTwentyOne==20)
+//{
+//    Console.WriteLine("True");
+//}
+//else
+//{
+//    Console.WriteLine("False");
+//}
+
+//22.Write a C# program to check if an given integer is within 20 of 100 or 200
+//Step 1 create variable Done
+//int exerciseTwentytwo = 900;
+////Step 2 condition Within 20 of 100 (for the given integer) Is 90 within 20 of 100. 
+
+//if (exerciseTwentytwo>=80 && exerciseTwentytwo<=120 || exerciseTwentytwo>=180 && exerciseTwentytwo<=220)
+//{
+//    Console.WriteLine(exerciseTwentytwo+ " is within 20 of 100 or 200");
+//}
+//else
+//{
+//    Console.WriteLine(exerciseTwentytwo+ " is not within 20 of 100 or 200");
+//}
+
+//23.Write a C# program to convert a given string into lowercase.
+
+//Step 1 Create string variable
+
+string exerciseTwentythree = "SINGlE MALT LIVES PODCAST";
+
+Console.WriteLine(exerciseTwentythree.ToLower());
+
